80ef4e28583fbebf47a10d00512e7e5a8308b958
Nfon-Carl <nfoncarl48@gmail.com>
2023-07-02 23:14:34 +0100

    started FizzBuzz challenge

--- one.js
+++ one.js
@@ -1,18 +1,44 @@
-//control flow
-// leap year challenge
+// //control flow
+// // leap year challenge
 
-a = prompt("Imput year");
+// a = prompt("Imput year");
 
-if (a % 4 === 0) {
-  if (a % 100 === 0) {
-    if (a % 400 === 0) {
-      console.log("Leap Year");
-    } else {
-      console.log("Not a Leap");
-    }
+// if (a % 4 === 0) {
+//   if (a % 100 === 0) {
+//     if (a % 400 === 0) {
+//       console.log("Leap Year");
+//     } else {
+//       console.log("Not a Leap");
+//     }
+//   } else {
+//     console.log("Leap Year");
+//   }
+// } else {
+//   console.log("Not a Leap");
+// }
+
+let a = [];
+let count = 1;
+function fizzBuzz() {
+  //code
+
+  if (a % 3 === 0) {
+    a.push("Fizz");
+  } else if (a % 5 === 0) {
+    a.push("Buzz");
+  } else if ((a % 3 === 0) & (a % 5 === 0)) {
+    a.push("fizzBuzz");
   } else {
-    console.log("Leap Year");
+    a.push(count);
   }
-} else {
-  console.log("Not a Leap");
+
+  count++;
+  console.log(a);
 }
+
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
+fizzBuzz();
