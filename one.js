827d6cb8ae418aa0eb59673134c1fe34ca07660e
Nfon-Carl <nfoncarl48@gmail.com>
2023-07-02 22:47:19 +0100

    leap year challenge

--- one.js
+++ one.js
@@ -1,6 +1,18 @@
-let a = Math.random();
-console.log(a);
+//control flow
+// leap year challenge
 
-a = a * 10;
-a = sa.Math.floor(a);
-console.log(a);
+a = prompt("Imput year");
+
+if (a % 4 === 0) {
+  if (a % 100 === 0) {
+    if (a % 400 === 0) {
+      console.log("Leap Year");
+    } else {
+      console.log("Not a Leap");
+    }
+  } else {
+    console.log("Leap Year");
+  }
+} else {
+  console.log("Not a Leap");
+}
